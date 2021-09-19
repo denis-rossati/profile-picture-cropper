bacdc33a7f8e0d34e95559b71482ebce5539cd69
denis-rossati <denis.rossati@hotmail.com>
2021-09-19 16:06:39 -0300

    adding icon and button with image in restart process icon

---
+++ src/components/RestartProcess.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import closeIcon from '../icons/closeX.png';
+
+export const RestartProecss = () => {
+  return(
+    <div>
+      <button>
+        <img src={closeIcon} alt="close icon" />
+      </button>
+    </div>
+  )
+};
