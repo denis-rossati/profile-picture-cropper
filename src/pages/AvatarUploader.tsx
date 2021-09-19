ed628e7a8598938bcba27710e05fd89f5cf05583
denis-rossati <denis.rossati@hotmail.com>
2021-09-19 13:16:28 -0300

    adding pages folder and start implementation of AvatarUploader component

---
+++ src/pages/AvatarUploader.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AvatarUploader = (): JSX.Element => {
+  const renderComponent = (): JSX.Element => {
+    return(<p>oi</p>);
+  }
+
+  return (
+    <main>
+      { renderComponent() }
+    </main>
+  );
+}
+
+export default AvatarUploader;
