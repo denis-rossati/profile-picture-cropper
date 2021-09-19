165fd9a2dfce96bda5497d5223796b5c0d2da9a6
denis-rossati <denis.rossati@hotmail.com>
2021-09-19 13:24:46 -0300

    adding state to image

--- src/pages/AvatarUploader.tsx
+++ src/pages/AvatarUploader.tsx
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 
 const AvatarUploader = (): JSX.Element => {
   const [componentName, setComponentName] = useState('LogoInput');
+  const [logoImg, setLogoImg] = useState<null | string>(null);
+
   const renderComponent = (): JSX.Element => {
     if (componentName === 'LogoInput') {
       return(<p>hello</p>);
     }
-    
+
   return(<p>hi</p>);
   }
 
