432bc797e2ac3d5e3e6698430bb0d9275bc8e8e1
denis-rossati <denis.rossati@hotmail.com>
2021-09-19 13:52:22 -0300

    creating function to receive file from drop event

--- src/components/LogoInput.tsx
+++ src/components/LogoInput.tsx
@@ -7,16 +7,27 @@ type Props = {
 export const LogoInput = ({
   logoImg,
 }: Props): JSX.Element => {
+
+  const handleOnDropUpload = async (event: React.DragEvent) => {
+    event.preventDefault();
+    const file = event.dataTransfer.files[0];
+    await manageAvatarState(file);
+  }
+
+  const manageAvatarState = async (file: File) => {
+
+  }
+
   return (
     <>
       { logoImg && <img id="logoCropped" src={logoImg} alt="logo frame" />}
       <section
-        onDragOver={(e) => e.preventDefault()}
-        onDragEnter={(e) => e.preventDefault()}
-        onDragLeave={(e) => e.preventDefault()}
+        onDragOver={(event) => event.preventDefault()}
+        onDragEnter={(event) => event.preventDefault()}
+        onDragLeave={(event) => event.preventDefault()}
         onDrop={handleOnDropUpload}
       >
-        <p>iae</p>
+        <p>dropa uma imagem em mim</p>
       </section>
     </>
   );
