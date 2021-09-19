d9232494334ce2f0485250f5871bb7c32f240e71
denis-rossati <denis.rossati@hotmail.com>
2021-09-19 15:54:19 -0300

    creating skeleton of the component

---
+++ src/components/ErrorComponent.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const ErrorComponent = () => {
+  return (
+    <div>
+      <article>
+        <div>
+          <img src={}  alt="atention icon" />
+        </div>
+        <div>
+          <p>Sorry, the upload failed.</p>
+          <button onClick={}>Try again</button>
+        </div>
+      </article>
+    </div>
+  );
+};
