fea5556e9c568b8ded2aa23bc7bbae0e5337c52d
denis-rossati <denis.rossati@hotmail.com>
2021-09-19 13:40:49 -0300

    add event to drop event

--- src/components/LogoInput.tsx
+++ src/components/LogoInput.tsx
@@ -10,6 +10,14 @@ export const LogoInput = ({
   return (
     <>
       { logoImg && <img id="logoCropped" src={logoImg} alt="logo frame" />}
+      <section
+        onDragOver={(e) => e.preventDefault()}
+        onDragEnter={(e) => e.preventDefault()}
+        onDragLeave={(e) => e.preventDefault()}
+        onDrop={handleOnDropUpload}
+      >
+        <p>iae</p>
+      </section>
     </>
   );
 };
